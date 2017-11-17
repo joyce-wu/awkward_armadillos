f09c0f3cdd90b5b98245e02b982e79ff306db175
tmoi29 <tmoi@stuy.edu>
2017-11-16 19:59:26 -0500

    stuff

--- utils/nyt_process.py
+++ utils/nyt_process.py
@@ -1,21 +1,54 @@
 import urllib2, json
+import data_scraper as scraper
 
 nyt_base="https://api.nytimes.com/svc/movies/v2/reviews/search.json?api-key="
 
-f = open("../nyt_key.txt", "r")
-key = f.read()
+f = open("../key.txt", "r")
+txt = f.read()
+key = txt.split('\n')[0]
 
 #Keep track of how often this function is called!
 #DON'T go over quotas!!
 
-def access_url(query = ""):
+def access_url(query):
     acc = nyt_base + key
-    if (query = ""):
-        data = urllib2.urlopen(acc)
-        d = json.loads(data.read())
-        return d
-
-#Wrapper for NYT search function
-def get_movies(query):
-    d = access_url(nyt_base, nyt_key, "query", query)
+    if (query == ""):
+        try:
+            data = urllib2.urlopen(acc)
+            d = json.loads(data.read())
+            return d
+        except:
+            print "your key is wrong or you have reached your monthy quota"
     
+    else:
+       try:
+           acc += "&query="
+           q = query.replace(' ', "+")
+           acc += q
+           data = urllib2.urlopen(acc)
+           d = json.loads(data.read())
+           return d
+       except:
+           print "your key is wrong or you have reached your monthy quota"
+           
+
+def get_title(query=""):
+    d = access_url(query)
+    print  d['results'][0]["display_title"]
+    return d['results'][0]["display_title"]
+
+def get_review(query):
+    if query =="":
+        print "give me a title"
+    else:
+        try:
+            d = access_url(query)
+            link = d['results'][0]['link']['url']
+            return scraper.scrape(link)
+        except:
+            print "No review found"
+
+
+#TESTING...
+#print get_review('silence of the lambs')
+#works
