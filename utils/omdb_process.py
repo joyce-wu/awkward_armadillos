820466df86472172c557af10821f18287ce6ed6d
tmoi29 <tmoi@stuy.edu>
2017-11-16 20:00:44 -0500

    key is wrong

--- utils/omdb_process.py
+++ utils/omdb_process.py
@@ -1,19 +1,39 @@
 import urllib2, json
+import nyt_process as nyt
 
-nyt_key="46da8b1424754c3b98e0dcb908aaca8a"
-nyt_base="https://api.nytimes.com/svc/movies/v2/reviews/search.json"
-mvdb_key="de1ce232"
-mvdb_base=""
+
+omdb_base="http://www.omdbapi.com/?apikey="
+
+
+f = open("../key.txt", "r")
+txt = f.read()
+key = txt.split('\n')[1]
 
 #Keep track of how often this function is called!
 #DON'T go over quotas!!
-def access_url(url, key, arg, value):
-    acc = url + "api-key=" + key + "&" + arg + "=" + value
-    data = urllib2.urlopen(acc)
-    d = json.loads(data.read())
+
+def access_url(query):
+    acc = omdb_base + key
+    if (query == ""):
+        print "give me something to search for"
+    
+    else:
+       try:
+           acc += "&t="
+           q = query.replace(' ', "+")
+           acc += q
+           data = urllib2.urlopen(acc)
+           d = json.loads(data.read())
+           return d
+       except:
+           print "your key is wrong or you have reached your monthy quota1"
+           
+
+#Wrapper for OMDB search function
+def get_movies(query=""):
+    q = nyt.get_title(query)
+    d = access_url(q)
     return d
 
-#Wrapper for NYT search function
-def get_movies(query):
-    d = access_url(nyt_base, nyt_key, "query", query)
+#print get_movies('silence of the lambs')
     
