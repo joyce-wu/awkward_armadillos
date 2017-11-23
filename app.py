6714de68795cf2b5b76f05d08de0945bd2ffb8fb
jenyu7 <jyu7@stuy.edu>
2017-11-23 08:44:03 -0500

    added login/logout/signup functionality and database

--- app.py
+++ app.py
@@ -3,21 +3,68 @@ awkward_armadillos
 P #1: arRESTed development
 Kelly Wang, Tiffany Moi, Joyce Wu, Jen Yu
 '''
-
-from flask import Flask, render_template, request
-from utils import nyt_process, omdb_process
+import os
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+from utils import nyt_process, omdb_process, auth
 import urllib2, json
 
 app = Flask(__name__)
+app.secret_key = os.urandom(32)
 
 @app.route("/")
 def start():
-    return render_template('home.html')
+    if session.get('username'):
+        return render_template('base.html', loggedIn=True)
+    return render_template('base.html', loggedIn=False)
+
+# Login Authentication
+@app.route('/login', methods=['GET', 'POST'])
+def authentication():
+    # if user already logged in, redirect to homepage(base.html)
+    if session.get('username'):
+        return redirect('profile')
+    # user entered login form
+    elif request.form.get('login'):
+        print "login"
+        return auth.login()
+    # user didn't enter form
+    else:
+        return render_template('login.html')
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def crt_acct():
+    if session.get('username'):
+        return redirect('base')
+    # user entered signup form
+    elif request.form.get('signup'):
+        return auth.signup()
+    else:
+        return render_template('signup.html')
+
+# Profile page - shows profile stats and (if time, allow them to change password)
+@app.route('/profile', methods=['GET', 'POST'])
+def profile():
+    if not session.get('username'):
+        flash("Not logged in")
+        return redirect(url_for('authentication'))
+    else:
+        return render_template('profile.html', user=session.get('username'), loggedIn=True)
+
+# Logging out
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+    if not session.get('username'):
+        flash("Not logged in")
+    else:
+        flash("Logged out")
+        session.pop('username')
+        return redirect(url_for('authentication'))
 
 @app.route("/search", methods=['POST', 'GET'])
 def search():
-    movie_list = nyt_process.get_title(request.args["title"])
-    return render_template("all_movies.html", movies = movie_list)
+    movie_list = nyt_process.search_results(request.args["title"])
+    return render_template("search_res.html", movies = movie_list)
 
 @app.route("/movie_review", methods=['POST', 'GET'])
 def get_movie():
