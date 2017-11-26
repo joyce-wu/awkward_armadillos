cfab99f09e42b5056d47d19a7db72aeaa6526ea8
Kelly Wang <kwang7@stuy.edu>
2017-11-25 23:59:52 -0500

    yeehaw

--- utils/data_scraper.py
+++ utils/data_scraper.py
@@ -10,16 +10,17 @@ def scrape(url):
     tree = lxml.html.fromstring(r.text)
 
     # print the parsed DOM Tree
-
+    
     # construct a CSS Selector
     sel = CSSSelector('p')
 
     # Apply the selector to the DOM tree.
     results = sel(tree)
 
-
     # get the text out of all the results
     data = [result.text for result in results]
-
+    print data
     #returns a list of text
     return data
+
+scrape('http://www.nytimes.com/2017/11/23/movies/bombshell-the-hedy-lamarr-story-review-.html')
