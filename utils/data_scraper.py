0dd78c51517c9577c975e0387add4f06fdc3bb7d
Kelly Wang <kwang7@stuy.edu>
2017-11-26 12:57:41 -0500

    fixed data scrapingls!

--- utils/data_scraper.py
+++ utils/data_scraper.py
@@ -10,17 +10,34 @@ def scrape(url):
     tree = lxml.html.fromstring(r.text)
 
     # print the parsed DOM Tree
+    #print lxml.html.tostring(tree)
     
     # construct a CSS Selector
     sel = CSSSelector('p')
 
     # Apply the selector to the DOM tree.
     results = sel(tree)
-
+    
+    #print match.text
+    for num in range (0, len(results)):
+        match = results[num]
+        
+        if "<a href" in lxml.html.tostring(match):
+            index1 = lxml.html.tostring(match).index('<a href')
+            print index1
+            print lxml.html.tostring(match)[0:index1]
+            index2 = lxml.html.tostring(match).index('</a>')
+            print index2
+            print lxml.html.tostring(match)[index2:-1]
+            print "HELLLLLLLLLLOOOOOOOOOOOOOO"
+            result = lxml.html.tostring(match)[0:index1] + lxml.html.tostring(match)[index2:-1]
+            results[num] = lxml.html.fromstring(result)
+            print result
+            
     # get the text out of all the results
     data = [result.text for result in results]
     print data
     #returns a list of text
     return data
 
-scrape('http://www.nytimes.com/2017/11/23/movies/bombshell-the-hedy-lamarr-story-review-.html')
+scrape('https://www.nytimes.com/2017/11/16/movies/review-big-sonia-review-holocaust-survivor.html')
